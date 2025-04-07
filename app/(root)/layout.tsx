5b3410b494a8dce311b2309e39a46536338bf7ef
simo <msMarquez@mcm.edu.ph>
2025-04-07 18:01:22 +0800

    Full Authentication & Homepage

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,11 +1,24 @@
-import { ReactNode } from 'react'
+import { isAuthenticated } from "@/lib/actions/auth.action";
+import Image from "next/image";
+import Link from "next/link";
+import { redirect } from "next/navigation";
+import { ReactNode } from "react";
 
-const RootLayout = ({children}: {children: ReactNode}) => {
+const RootLayout = async ({ children }: { children: ReactNode }) => {
+  const isUserAuthenticated = await isAuthenticated();
+
+  if (!isUserAuthenticated) redirect("/sign-in");
   return (
-    <div>
+    <div className="root-layout">
+      <nav>
+        <Link href="/" className=" flex items-center gap-2">
+          <Image src="/logo.svg" alt="logo" width={38} height={32} />
+          <h2>AceIT</h2>
+        </Link>
+      </nav>
       {children}
     </div>
-  )
-}
+  );
+};
 
-export default RootLayout
+export default RootLayout;
---
+++ firebase/client.ts
@@ -0,0 +1,19 @@
+import { initializeApp, getApp, getApps } from "firebase/app";
+import { getAuth } from 'firebase/auth';
+import {getFirestore} from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyADt4qfw0yOSjcS3HCpzVfHhhv9L9A5Y8Y",
+  authDomain: "aceit-4a85f.firebaseapp.com",
+  projectId: "aceit-4a85f",
+  storageBucket: "aceit-4a85f.firebasestorage.app",
+  messagingSenderId: "893453845209",
+  appId: "1:893453845209:web:c5e15f4c4ac0dff33474c8",
+  measurementId: "G-33ZFEWY7SV"
+};
+
+// Initialize Firebase
+const app = !getApps.length ? initializeApp(firebaseConfig) : getApp();
+
+export const auth = getAuth(app)
+export const db = getFirestore(app)
